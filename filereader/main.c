46fda553872bdeeb0ab2076b7b946d99706a3b19
Dhruv Sengupta <dhruv.sengupta04@gmail.com>
2019-10-24 08:35:49 -0700

    Initial Commit

---
+++ filereader/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  filereader
+//
+//  Created by Sengupta, Dhruv on 10/24/19.
+//  Copyright © 2019 Sengupta, Dhruv. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
