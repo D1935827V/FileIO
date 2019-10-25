96917dd47e342620df5fbe2c7919ac033913d8c2
Dane <dvestal2012@gmail.com>
2019-10-25 08:33:39 -0700

    ao

--- filereader/main.c
+++ filereader/main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 int main() {
-
+   
+    int x = 2;
    FILE *fp;
    char buff[255];
 
-   fp = fopen("/Users/1855144/Documents/bruh.txt", "r");
+   fp = fopen("/Users/1935827/Documents/bruh.txt", "r");
    fscanf(fp, "%s", buff);
    printf("1 : %s\n", buff );
 
+    while (x < 100){
    fgets(buff, 255, (FILE*)fp);
-   printf("2: %s\n", buff );
-   
-   fgets(buff, 255, (FILE*)fp);
-   printf("3: %s\n", buff );
+   printf("%i: %s\n",x, buff);
+        x++;
+}
+
    fclose(fp);
 
 }
+
